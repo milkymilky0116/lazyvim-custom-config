3835f0e08f8e019a957d9d2ef7fbae90b007b8c7
milkymilky0116 <sjlee990129@gmail.com>
2024-01-08 19:34:31 +0900

    update config : htmx, templ, python

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,38 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {},
+      ruff_lsp = {
+        keys = {
+          {
+            "<leader>co",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.organizeImports" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Organize Imports",
+          },
+        },
+      },
+      htmx = {
+        filetypes = { "html", "templ" },
+      },
+    },
+    setup = {
+      ruff_lsp = function()
+        require("lazyvim.util").lsp.on_attach(function(client, _)
+          if client.name == "ruff_lsp" then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end
+        end)
+      end,
+    },
+  },
+}
