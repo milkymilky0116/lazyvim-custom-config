36cfe8cc9df874fe8b23828cd7b5019d5b54df7c
milkymilky0116 <sjlee990129@gmail.com>
2024-01-20 15:23:41 +0900

    update config: rust

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -23,6 +23,59 @@ return {
       htmx = {
         filetypes = { "html", "templ" },
       },
+      tailwindcss = {
+        filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+        init_options = { userLanguages = { templ = "html" } },
+      },
+      rust_analyzer = {
+        keys = {
+          {
+            "<leader>cR",
+            function()
+              vim.cmd.RustLsp({ "hover", "actions" })
+            end,
+            desc = "Hover Actions (Rust)",
+          },
+        },
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      },
+    },
+    taplo = {
+      keys = {
+        {
+          "K",
+          function()
+            if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+              require("crates").show_popup()
+            else
+              vim.lsp.buf.hover()
+            end
+          end,
+          desc = "Show Crate Documentation",
+        },
+      },
     },
     setup = {
       ruff_lsp = function()
